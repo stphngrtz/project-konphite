f48507321d1d89d297817c939db1eae7e391b31d
stphngrtz <stephan.goertz@gmail.com>
2022-12-01 19:32:16 +0100

    initialized with empty project

---
+++ todo-service/README.md
@@ -0,0 +1,7 @@
+# TODO Service
+...
+
+```bash
+./gradlew build
+java -jar build/libs/todo-service-0.0.1-SNAPSHOT.jar
+```
---
+++ todo-service/src/test/kotlin/de/stphngrtz/konphite/todo/TodoServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package de.stphngrtz.konphite.todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
