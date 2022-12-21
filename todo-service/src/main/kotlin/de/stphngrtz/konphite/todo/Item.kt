468d28a4c8881df6f6d6349d691ad451053f4028
stphngrtz <stephan.goertz@gmail.com>
2022-12-21 22:10:14 +0100

    replaced array list with repository

--- todo-service/src/main/kotlin/de/stphngrtz/konphite/todo/Item.kt
+++ todo-service/src/main/kotlin/de/stphngrtz/konphite/todo/Item.kt
@@ -1,3 +1,6 @@
 package de.stphngrtz.konphite.todo
 
+import org.springframework.data.redis.core.RedisHash
+
+@RedisHash("Item")
 data class Item(val id: String?, val subject: String, val done: Boolean)
--- todo-service/src/main/kotlin/de/stphngrtz/konphite/todo/TodoServiceApplication.kt
+++ todo-service/src/main/kotlin/de/stphngrtz/konphite/todo/TodoServiceApplication.kt
@@ -1,10 +1,23 @@
 package de.stphngrtz.konphite.todo
 
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class TodoServiceApplication
+class TodoServiceApplication {
+
+    @Bean
+    fun init(repository: ItemRepository) = ApplicationRunner {
+        if (!repository.existsById("1"))
+            repository.save(Item("1", "first example", false))
+        if (!repository.existsById("2"))
+            repository.save(Item("2", "second example", true))
+        if (!repository.existsById("3"))
+            repository.save(Item("3", "third example", false))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<TodoServiceApplication>(*args)
