031555cd7def406c4d27caf98f99f095b1a80b67
stphngrtz <stephan.goertz@gmail.com>
2022-12-23 11:38:43 +0100

    added config module

--- bookmark-service/src/prisma/prisma.service.ts
+++ bookmark-service/src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: 'postgresql://postgres:super_secret_pw@localhost:5432/bookmarks?schema=public'
+                    url: config.get('DATABASE_URL')
                 }
             }
         })
